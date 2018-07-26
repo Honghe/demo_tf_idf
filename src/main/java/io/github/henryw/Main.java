657e94915ef66583db70e3b60ba103db2e0d6c46
Honghe Wu <leopardsaga@gmail.com>
2018-07-26 15:14:41 +0800

    refactor api.

--- src/main/java/io/github/henryw/Main.java
+++ src/main/java/io/github/henryw/Main.java
@@ -12,36 +12,12 @@ public class Main {
     private static Logger LOGGER = Logger.getLogger(Class.class.getName());
 
     public static void main(String[] args) {
-        String[] corpus = {"这  是  ; ； 中文",
-                "测 试 中  a 文字"};
+        String[] corpus = {
+                "这  是  ; ； 中文这个字",
+                "测试 a中文字"};
         //    segmentation
-        Set<String> stopwords = new ResourceReader().getStopwords("my_stopwords.txt");
-        LOGGER.info(Arrays.toString(stopwords.toArray()));
-
-        List<List<String>> documents = new ArrayList<List<String>>();
-        for (int i = 0; i < corpus.length; i++) {
-//            documents.add(ToAnalysis.parse(corpus[i]).getTerms());
-            List<Term> terms = ToAnalysis.parse(corpus[i]).getTerms();
-            List<String> termsSeg = new ArrayList<String>();
-            for (Iterator<Term> it = terms.iterator(); it.hasNext(); ) {
-                String termName = it.next().getName().trim();
-                // stop word filter
-                if (!stopwords.contains(termName) && termName.length()>0) {
-                    termsSeg.add(termName);
-                }
-            }
-            documents.add(termsSeg);
-        }
-
-        //    Count
-        CountVectorizer countVectorizer = new CountVectorizer();
-        List<List<Integer>> vectors = countVectorizer.fit_transform(documents);
-        List<String> feature_names = countVectorizer.get_feature_names();
-
-        Printer.print2dArrayInt(vectors);
-        LOGGER.info(feature_names.toString());
-        //    if-idf
-        double[][] tfidf = new TFIDFCalculator().fit_transform(vectors);
+        double[][] tfidf = new TFIDFExtractor().fit_transform(corpus);
         Printer.print2dArray(tfidf);
     }
+
 }
---
+++ src/main/java/io/github/henryw/TFIDFExtractor.java
@@ -0,0 +1,47 @@
+package io.github.henryw;
+
+import io.github.henryw.utils.Printer;
+import io.github.henryw.utils.ResourceReader;
+import org.ansj.domain.Term;
+import org.ansj.splitWord.analysis.ToAnalysis;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Set;
+import java.util.logging.Logger;
+
+public class TFIDFExtractor {
+    private static Logger LOGGER = Logger.getLogger(Class.class.getName());
+
+    public double[][] fit_transform(String[] corpus) {
+
+        Set<String> stopwords = new ResourceReader().getStopwords("my_stopwords.txt");
+
+        List<List<String>> documents = new ArrayList<List<String>>();
+        for (int i = 0; i < corpus.length; i++) {
+//            documents.add(ToAnalysis.parse(corpus[i]).getTerms());
+            List<Term> terms = ToAnalysis.parse(corpus[i]).getTerms();
+            List<String> termsSeg = new ArrayList<String>();
+            for (Iterator<Term> it = terms.iterator(); it.hasNext(); ) {
+                String termName = it.next().getName().trim();
+                // stop word filter
+                if (!stopwords.contains(termName) && termName.length() > 0) {
+                    termsSeg.add(termName);
+                }
+            }
+            documents.add(termsSeg);
+        }
+
+        //    Count
+        CountVectorizer countVectorizer = new CountVectorizer();
+        List<List<Integer>> vectors = countVectorizer.fit_transform(documents);
+        List<String> feature_names = countVectorizer.get_feature_names();
+
+        Printer.print2dArrayInt(vectors);
+        LOGGER.info(feature_names.toString());
+        //    if-idf
+        double[][] tfidf = new TFIDFCalculator().fit_transform(vectors);
+        return tfidf;
+    }
+}
